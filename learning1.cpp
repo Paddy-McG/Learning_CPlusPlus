5b784331b008648af8834c86684f7025783fb49e
Paddy-McG <120601884+Paddy-McG@users.noreply.github.com>
2023-10-03 13:36:19 +0000

    Learning hoe to take user input from the console

--- learning1.cpp
+++ learning1.cpp
@@ -9,15 +9,28 @@ int main ()
     int a = 5;
     int b = 8; 
 
+    //Learning how to find the bigger of two numbers
+
     int maxVal = std::max(a,b);
     std::cout << "The highest value is " <<maxVal<<std::endl;
+
+    //Learning how to find the mininmalvalue of two
 
     int minVal = std::min(a,b);
     std::cout << "The lowest value if the two digits is " << minVal << std::endl;
 
+    //Learning how to make a char a capital
+
     char ch = 'a';
     char upperCaseChar = std::toupper(ch);
     std::cout << "The uppercase of the character a is " << upperCaseChar << std::endl;
+
+    //Learning how to take input from the console
+
+    std::cout << "Enter a number: ";
+    int num;
+    std::cin >> num;
+    std::cout << "The number you have entered is " << num << std::endl;
 
     return 0;
 }
