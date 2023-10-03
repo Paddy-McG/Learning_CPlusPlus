cf2c3f8164437fdc67deb11f376c59e8c4cdcc26
Paddy-McG <120601884+Paddy-McG@users.noreply.github.com>
2023-10-03 13:17:44 +0000

    Checking for the highest value of 2 digits

--- learning1.cpp
+++ learning1.cpp
@@ -3,6 +3,14 @@
 
 int main () 
 {
+    //Printing to the screen
     std::cout << "Hello World!" << std::endl;
+
+    int a = 5;
+    int b = 8; 
+
+    int maxVal = std::max(a,b);
+    std::cout << "The highest value is " <<maxVal<<std::endl;
+
     return 0;
 }
